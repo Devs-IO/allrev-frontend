26f7fc714d85fa688e813b370f4f8facee2a681a
rebecanonato89 <rebecanonato89@gmail.com>
2025-07-21 16:42:30 -0300

    feat: env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { JwtModule, JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { jwtInterceptor } from './app/core/interceptors/jwt.interceptor';
+import { environment } from './environments/environment';
 import { provideStore } from '@ngrx/store'; // Supondo que você tenha um interceptor
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appReducers } from './app/core/state/app.state';
@@ -18,9 +23,9 @@ import { PhoneMaskPipe } from './app/core/pipe/phone-mask.pipe';
 // Função para fornecer as opções de configuração do JWT
 export function jwtOptionsFactory() {
   return {
-    tokenGetter: () => localStorage.getItem('token'), // Recuperar o token do localStorage
-    allowedDomains: ['localhost:3000'], // Domínios permitidos
-    disallowedRoutes: ['http://localhost:3000/auth/login'], // Rotas que não precisam de autenticação
+    tokenGetter: () => localStorage.getItem('token'),
+    allowedDomains: environment.apiUrl,
+    disallowedRoutes: environment.apiUrl + '/auth/login',
   };
 }
 
@@ -38,9 +43,9 @@ bootstrapApplication(AppComponent, {
     FormsModule, // Formulários template-driven
     ReactiveFormsModule, // Formulários reativos
     {
-        provide: JWT_OPTIONS,
-        useValue: JWT_OPTIONS, // Usar o valor padrão
-        useFactory: jwtOptionsFactory,
+      provide: JWT_OPTIONS,
+      useValue: JWT_OPTIONS, // Usar o valor padrão
+      useFactory: jwtOptionsFactory,
     }, // Configuração do JWT_OPTIONS
     JwtModule, // Registrar o JwtModule diretamente, sem .forRoot
     JwtHelperService,
